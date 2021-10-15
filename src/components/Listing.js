21eb95fdfb8da73d6a31ebef3aaeb58dc5739198
Umair115 <umair42795@gmail.com>
2021-10-15 20:38:49 +0500

    linked with api

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 function Listing(props){
 
+    const [data,setData] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+    const temp_name = props.heading == 'Cricket' ? 'match' : 'movie';
+    const getData = async() => {
+        const url = '/api/all-' + (props.heading == 'Cricket' ? 'matches' : 'movies');
+        console.log(url)
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data)
+        setData(data.data);
+        setLoading(false);
+    }
+
+
+    React.useEffect(()=>{
+        getData();
+    },[])
+
     var matches = [
         {
             id : 1,
@@ -88,44 +106,26 @@ function Listing(props){
             </div>
             <div className="row">
                 {
-                    props.heading == 'Cricket' ? 
-                    matches.map(match => {
+                    loading ? <h1>Loading</h1> : 
+                    data.map(_data => {
                         return(
                             <div className="col-lg-4 col-md-6 col-12">
                             <div className="destination-box">
                                 <div className="destination-img">
-                                    <img src={require('../images/'+match.match_image).default} alt="Destination"/>
+                                    {/* <img src={require('../images/'+_data.match_image).default} alt="Destination"/> */}
                                 </div>
                                 <div className="destination-content">
                                     <div className="primary-heading color-light">
-                                        <h5>{match.match_name}</h5>
-                                        <p>{match.match_details}</p>
-                                        <a href="cricket-details.html" className="primary-btn primary-bg">Match Details</a>
+                                        <h5>{_data[temp_name+'_name']}</h5>
+                                        <p>{_data[temp_name+'_name']}</p>
+                                        {/* <a href="cricket-details.html" className="primary-btn primary-bg">Match Details</a> */}
                                     </div>
                                 </div>
                             </div>
                         </div>
                         )
                     })
-                    :
-                    movies.map(movie => {
-                        return(
-                            <div className="col-lg-4 col-md-6 col-12">
-                            <div className="destination-box">
-                                <div className="destination-img">
-                                    <img src={require('../images/'+movie.movie_image).default} alt="Destination"/>
-                                </div>
-                                <div className="destination-content">
-                                    <div className="primary-heading color-light">
-                                        <h5>{movie.movie_name}</h5>
-                                        <p>{movie.movie_details}</p>
-                                        <a href="cricket-details.html" className="primary-btn primary-bg">Movie Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        )
-                    })
+                    
 
                 }
                    
